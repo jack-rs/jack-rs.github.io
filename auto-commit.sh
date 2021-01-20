f09291aded5eeedf84182db24aee8aef80224ab2
JACKITO <master.deigoo@gmail.com>
2021-01-20 19:03:12 +0700

    Changed file , Auto-Commit V0.1

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#build
+yarn build
+
+echo "--- Build Thành Công"
+#auto commit
 git add --all
 git commit -am "Changed file $*, Auto-Commit V0.1"
 git push origin master
+
+echo "--- Deploy thành công"
