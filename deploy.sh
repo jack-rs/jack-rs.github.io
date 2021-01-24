bde6f1cf21ebc74affb3effe22e35edee5a00798
JACKITO <master.deigoo@gmail.com>
2021-01-24 12:40:22 +0700

    cap nhat
    dsads || ''

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Make by JackRs
+
+echo "Chọn thao tác commit:"
+# update : cap nhat gi
+select actions in update publish; do
+    # if/else select
+    if [[ $actions == publish ]]; then
+        echo -n "Tiêu đề commit: "
+        read commit
+        echo -n "Mô tả commit: "
+        read desc
+        git add .
+        git commit -m "$commit" -m "$desc || ''"
+        # deploy with git
+        # step 1
+        yarn deploy
+        # step 2
+        git add .
+        git commit -m "publish"
+        git push origin ecrm-website
+        #close
+        echo "xuat ban"
+        exit
+    else
+        echo -n "Tiêu đề commit: "
+        read commit
+        echo -n "Mô tả commit: "
+        read desc
+        git add .
+        git commit -m "$commit" -m "$desc || ''"
+        git push origin ecrm-website
+        #close
+        echo "cap nhat"
+        exit
+    fi
+done
